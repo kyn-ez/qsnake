d2e3299653b6c7f23b6b40f97344ed87e29d4fb3
kynez <kynez.dev@gmail.com>
2025-02-23 17:57:25 +0100

    Improve code structure

--- qsnake.c
+++ qsnake.c
@@ -2,28 +2,70 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-static SDL_Window* window = NULL;
-static SDL_Renderer* renderer = NULL;
+// Game settings
+const Uint64 UPDATE_RATE_IN_MS = 125;
+const int WINDOW_SIDE = 600;
+const int CELLS_IN_WINDOW_SIDE = 4;
+const int CELL_SIDE = WINDOW_SIDE / CELLS_IN_WINDOW_SIDE;
+
+// Describes the current status of the game
+typedef struct {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    Uint64 last_update_time;
+} State;
 
 // This function runs once at startup
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
+    // Initialize the SDL library
+    bool ok = SDL_Init(SDL_INIT_VIDEO);
+    if (!ok) {
+        SDL_Log("Couldn't initialize the SDL video subsystem: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // Allocate enough memory for the game state
+    State* state = (State*)SDL_calloc(1, sizeof(State));
+    if (!state) {
+        SDL_Log("Coudn't allocate memory for the game state: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // Set the SDL appstate to be our own custom game state object
+    *appstate = state;
+
     // Create the window
-    bool ok = SDL_CreateWindowAndRenderer("QSnake", 800, 600, SDL_WINDOW_VULKAN, &window, &renderer);
+    ok = SDL_CreateWindowAndRenderer("QSnake", WINDOW_SIDE, WINDOW_SIDE, SDL_WINDOW_VULKAN, &state->window, &state->renderer);
     if (!ok) {
         SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
+    state->last_update_time = SDL_GetTicks();
     return SDL_APP_CONTINUE;
+}
+
+// This function runs once at shutdown
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+    if (appstate) {
+        State* state = (State*)appstate;
+        SDL_DestroyRenderer(state->renderer);
+        SDL_DestroyWindow(state->window);
+        SDL_free(state);
+    }
 }
 
 // This function runs when a new event (mouse input, keypresses, etc) occurs
 SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 {
-    if (event->type == SDL_EVENT_KEY_DOWN || event->type == SDL_EVENT_QUIT) {
-        // End the program with a success status code
-        return SDL_APP_SUCCESS;
+    switch (event->type) {
+        case SDL_EVENT_QUIT:
+            // End the program with a success status code
+            return SDL_APP_SUCCESS;
+        case SDL_EVENT_KEY_DOWN:
+            return SDL_APP_SUCCESS;
     }
 
     return SDL_APP_CONTINUE;
@@ -32,34 +74,34 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 // This function runs once per frame
 SDL_AppResult SDL_AppIterate(void* appstate)
 {
-    const char* message = "Hello, World!";
+    State* state = (State*)appstate;
+    const Uint64 now_time = SDL_GetTicks();
 
-    // Get the output size in pixels
-    int w = 0, h = 0;
-    SDL_GetRenderOutputSize(renderer, &w, &h);
-
-    // Set the rendering scale
-    const float scale = 4.0f;
-    SDL_SetRenderScale(renderer, scale, scale);
-
-    // Center the message
-    float x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(message)) / 2;
-    float y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
+    // Compute the game logic for each fixed amout of time passed since last update
+    while ((now_time - state->last_update_time) >= UPDATE_RATE_IN_MS) {
+        state->last_update_time += UPDATE_RATE_IN_MS;
+    }
 
     // Clear the screen
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(state->renderer, 0, 0, 0, 255);
+    SDL_RenderClear(state->renderer);
 
-    // Draw the text
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderDebugText(renderer, x, y, message);
+    // Initialize the object we will use to draw each cell of the grid
+    SDL_FRect rect;
+    rect.w = WINDOW_SIDE;
+    rect.h = WINDOW_SIDE;
+
+    // Draw the grid
+    SDL_SetRenderDrawColor(state->renderer, 255, 255, 255, 255);
+    for (int j = 0; j < CELLS_IN_WINDOW_SIDE; j++) {
+        for (int i = 0; i < CELLS_IN_WINDOW_SIDE; i++) {
+            rect.x = i * CELL_SIDE;
+            rect.y = j * CELL_SIDE;
+            SDL_RenderRect(state->renderer, &rect);
+        }
+    }
 
     // Update the screen
-    SDL_RenderPresent(renderer);
+    SDL_RenderPresent(state->renderer);
     return SDL_APP_CONTINUE;
-}
-
-// This function runs once at shutdown
-void SDL_AppQuit(void* appstate, SDL_AppResult result)
-{
 }
